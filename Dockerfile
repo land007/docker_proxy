dbbf6d15b00cd42c8d8d0fd45a71413c230384f8
阿七 <yiqiujia@hotmail.com>
2020-12-29 23:17:54 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM land007/ubuntu-node:latest
+
+MAINTAINER Jia Yiqiu <yiqiujia@hotmail.com>
+
+RUN echo $(date "+%Y-%m-%d_%H:%M:%S") >> /.image_times && \
+	echo $(date "+%Y-%m-%d_%H:%M:%S") > /.image_time && \
+	echo "land007/proxy" >> /.image_names && \
+	echo "land007/proxy" > /.image_name
+
+#RUN . $HOME/.nvm/nvm.sh && cd / && npm install basic-auth
+ADD node /node_
+
+ENV username=land007 \
+	password=fcea920f7412b5da7be0cf42b8c93759
+
+EXPOSE 8080
+EXPOSE 8888
+
+#CMD /check.sh /node && /node/start.sh
+
+#docker build -t land007/proxy:latest .
+#docker run -it --restart=always -e "username=land007" -e "password=81dc9bdb52d04dc20036dbd8313ed055" -p 8080:8080 -p 8888:8888 --name proxy land007/proxy:latest
---
+++ node/server.js
@@ -0,0 +1,109 @@
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var net = require('net');
+var url = require('url');
+const crypto = require('crypto');
+
+var username = process.env['username'] || 'land007';
+var password = process.env['password'] || '81dc9bdb52d04dc20036dbd8313ed055';
+
+function request(cReq, cRes) {
+	console.log('request.headers', cReq.headers);
+	const proxyAuth = cReq.headers['proxy-authorization'.toLowerCase()];
+	console.log('proxyAuth', proxyAuth);//bGFuZDAwNzoxMjM0
+	if (proxyAuth) {
+		const credentials = proxyAuth.replace('Basic', '');
+		const parsedCredentials = Buffer.from(credentials, 'base64').toString(); //converting from base64
+        const [user, pass] = parsedCredentials.split(':');
+        console.log(username, password);
+        const md5 = crypto.createHash('md5');
+        if (pass === undefined) {
+    		md5.update('undefined');
+    	} else {
+    		md5.update(pass);
+    	}
+    	var _pass = md5.digest('hex');
+    	if (user !== username || _pass !== password) {
+    		console.log(user, username, user !== username);
+    		console.log(_pass, password, _pass !== password);
+    		console.log('return');
+    		return;
+    	}
+	}
+	
+    var u = url.parse(cReq.url);
+
+    var options = {
+        hostname : u.hostname, 
+        port     : u.port || 80,
+        path     : u.path,       
+        method     : cReq.method,
+        headers     : cReq.headers
+    };
+
+    var pReq = http.request(options, function(pRes) {
+        cRes.writeHead(pRes.statusCode, pRes.headers);
+        pRes.pipe(cRes);
+    }).on('error', function(e) {
+        cRes.end();
+    });
+
+    cReq.pipe(pReq);
+}
+
+function connect(cReq, cSock) {
+	console.log('request.connect', cReq.headers);
+	const proxyAuth = cReq.headers['proxy-authorization'.toLowerCase()];
+	console.log('proxyAuth', proxyAuth);//bGFuZDAwNzoxMjM0
+	if (proxyAuth) {
+		const credentials = proxyAuth.replace('Basic', '');
+		const parsedCredentials = Buffer.from(credentials, 'base64').toString(); //converting from base64
+        const [user, pass] = parsedCredentials.split(':');
+        console.log(username, password);
+        const md5 = crypto.createHash('md5');
+        if (pass === undefined) {
+    		md5.update('undefined');
+    	} else {
+    		md5.update(pass);
+    	}
+    	var _pass = md5.digest('hex');
+    	if (user !== username || _pass !== password) {
+    		console.log(user, username, user !== username);
+    		console.log(_pass, password, _pass !== password);
+    		console.log('return');
+    		return;
+    	}
+	}
+	
+    var u = url.parse('http://' + cReq.url);
+
+    var pSock = net.connect(u.port, u.hostname, function() {
+        cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+        pSock.pipe(cSock);
+    }).on('error', function(e) {
+        cSock.end();
+    });
+
+    cSock.pipe(pSock);
+}
+
+process.on('uncaughtException', function(err) {
+    console.log(err.stack);
+    console.log('NOT exit...');
+});
+
+http.createServer()
+    .on('request', request)
+    .on('connect', connect)
+    .listen(8080, '0.0.0.0');
+
+var options = {
+    key: fs.readFileSync('./docx.qhkly.com_key.key'),
+    cert: fs.readFileSync('./docx.qhkly.com_chain.crt')
+};
+
+https.createServer(options)
+    .on('request', request)
+    .on('connect', connect)
+    .listen(8888, '0.0.0.0');
